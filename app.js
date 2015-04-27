8ba5ed8d127c6147cde06fb781bbd4ba0afac82d
Kevin Parrott <parrott.kevin@gmail.com>
2015-04-27 07:24:00 -0400

    Add async waterfall and series to requests in order to build up character profile.

--- app.js
+++ app.js
@@ -3,15 +3,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-
+var request = require('request');
+var async = require('async');
 //var routes = require('./config/routes');
 
 var app = express();
 
 // Enable CORS
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.set({
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    });
   next();
 });
 
@@ -19,6 +22,46 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/', express.static(path.join(__dirname, 'public/dist')));
+
+app.get('/api/searchDestinyPlayer', function(req, res) {
+
+  var bungieUrl = 'https://www.bungie.net/Platform/Destiny';
+  var membershipType = req.query.platform;
+  var displayName = req.query.name;
+  async.waterfall([
+    function(callback) {
+      var endpoint = '/SearchDestinyPlayer/' + membershipType + '/' + displayName;
+      request(bungieUrl + endpoint, function(error, response, body) {
+        var membershipId = JSON.parse(body).Response[0].membershipId;
+        callback(null, membershipId);
+      });
+    },
+    function(membershipId, callback) {
+      var endpoint = '/' + membershipType + '/Account/' + membershipId;
+      request(bungieUrl + endpoint, function(error, response, body) {
+        var accountInfo = JSON.parse(body).Response.data;
+        var characterIdArray = accountInfo.characters;
+        callback(null, membershipId, accountInfo, characterIdArray);
+      });
+    },
+    function(membershipId, accountInfo, characterIdArray, callback) {
+      var characterInventory = {};
+      async.each(characterIdArray, function(character, cb) {
+        var characterId = character.characterBase.characterId;
+        var endPoint = '/' + membershipType + '/Account/' + membershipId + '/character/' + characterId + '/inventory';
+        request(bungieUrl + endPoint, function (error, response, body) {
+          characterInventory[characterId] = JSON.parse(body).Response.data;
+          cb();
+        });
+      }, function() {
+          callback(null, accountInfo, characterInventory);
+      });
+    }
+  ], function(err, accountInfo, characterInventory) {
+    res.send([accountInfo, characterInventory]);
+  });
+
+});
 
 //app.use('/api', routes);
 
--- public/src/app/components/user/user.controller.js
+++ public/src/app/components/user/user.controller.js
@@ -2,17 +2,27 @@
   'use strict';
 
   angular
-    .module('user.controller', [])
+    .module('user.controller', ['ui.bootstrap'])
     .controller('UserController', UserController);
 
   UserController.$inject = ['$http'];
 
   function UserController($http) {
     var vm = this;
+    vm.bungie = 'https://www.bungie.net';
+    vm.user = {
+      platform: 1
+    };
 
-    vm.getUser = function(name) {
-      $http.get('https://www.bungie.net/Platform/Destiny/SearchDestinyPlayer/1/' + name).success(function(d){
+    vm.getUser = function() {
+      $http.get('/api/searchDestinyPlayer', {
+        params: {
+          platform: vm.user.platform,
+          name: vm.user.name
+        }
+      }).success(function(d){
         console.log(d);
+        vm.characters = d[0].characters;
       });
     };
   }
