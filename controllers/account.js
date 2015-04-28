c3ef6bddecda5373504deff6170cc8da50df91bb
Kevin Parrott <parrott.kevin@gmail.com>
2015-04-28 13:17:57 -0400

    Build out bungie endpoints for use

--- controllers/account.js
+++ controllers/account.js
@@ -6,7 +6,6 @@ var bungieEndpoints = require('../services/bungieEndpoints');
 
 var account = {
   get: function(req, res) {
-    var bungieUrl = 'https://www.bungie.net/Platform/Destiny';
     var membershipType = req.query.platform;
     var displayName = req.query.name;
     async.waterfall([
@@ -18,8 +17,8 @@ var account = {
         });
       },
       function(membershipId, callback) {
-        var endpoint = '/' + membershipType + '/Account/' + membershipId + '/?definitions=true';
-        request(bungieUrl + endpoint, function(error, response, body) {
+        var endpoint = bungieEndpoints.getAccount(membershipType, membershipId, true);
+        request(endpoint, function(error, response, body) {
           body = JSON.parse(body).Response;
           var accountInfo = {
             data: body.data,
@@ -33,8 +32,8 @@ var account = {
         var characterInventory = {};
         async.each(characterIdArray, function(character, cb) {
           var characterId = character.characterBase.characterId;
-          var endPoint = '/' + membershipType + '/Account/' + membershipId + '/character/' + characterId + '/inventory';
-          request(bungieUrl + endPoint, function (error, response, body) {
+          var endpoint = bungieEndpoints.getCharacterInventory(membershipType, membershipId, characterId, true);
+          request(endpoint, function (error, response, body) {
             characterInventory[characterId] = JSON.parse(body).Response.data;
             cb();
           });
--- services/bungieEndpoints.js
+++ services/bungieEndpoints.js
@@ -3,12 +3,27 @@
 var bungieUrl = 'https://www.bungie.net/Platform/Destiny';
 var endpoints = {
   searchDestinyPlayer: function(membershipType, displayName) {
-    var slugs = [bungieUrl, 'SearchDestinyPlayer', membershipType, displayName];
-    var endpoint = slugs.reduce(function(previousValue, currentValue) {
-      return previousValue + '/' + currentValue;
-    });
-    return endpoint;
+    var parts = [bungieUrl, 'SearchDestinyPlayer', membershipType, displayName];
+    return assembleUrl(parts);
+  },
+  getAccount: function(membershipType, membershipId, definitions) {
+    var parts = [bungieUrl, membershipType, 'Account', membershipId];
+    return assembleUrl(parts, definitions);
+  },
+  getCharacterInventory: function(membershipType, membershipId, characterId, definitions) {
+    var parts = [bungieUrl, membershipType, 'Account', membershipId, 'Character', characterId, 'Inventory'];
+    return assembleUrl(parts, definitions);
   }
 };
+
+function assembleUrl(parts, definitions) {
+  var endpoint = parts.reduce(function(previousValue, currentValue) {
+    return previousValue + '/' + currentValue;
+  });
+  if (definitions) {
+    endpoint += '/?definitions=true';
+  }
+  return endpoint;
+}
 
 module.exports = endpoints;
