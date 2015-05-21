413b06e57f332799cffada0480562a866508d057
Kevin Parrott <parrott.kevin@gmail.com>
2015-05-20 22:58:42 -0400

    changed port to 8080

--- bin/www.js
+++ bin/www.js
@@ -2,7 +2,7 @@
 
 var app = require('../app');
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 
 var server = app.listen(app.get('port'), function() {
   console.log('Server started on http://localhost:' + server.address().port);
