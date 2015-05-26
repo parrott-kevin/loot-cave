7921126624de1a89171e9b42cb847edb8b417cab
Kevin Parrott <parrott.kevin@gmail.com>
2015-05-26 13:38:21 -0400

    Updated lodash and added glimmer to display

--- public/src/app/components/account/account.controller.js
+++ public/src/app/components/account/account.controller.js
@@ -5,9 +5,11 @@
     .module('account.controller', ['ui.bootstrap'])
     .controller('AccountController', AccountController);
 
-  AccountController.$inject = ['accountResolver', 'equipped', 'definitionMatch'];
-  function AccountController(accountResolver, equipped, definitionMatch) {
+  AccountController.$inject = ['accountResolver', 'equipped', 'definitionMatch', '_'];
+  function AccountController(accountResolver, equipped, definitionMatch, _) {
     var vm = this;
+    vm.account = _.omit(accountResolver.data, 'characters');
+    vm.account.inventory.currencies[0] = definitionMatch.get(vm.account.inventory.currencies[0], accountResolver.definitions);
     vm.bungie = 'https://www.bungie.net';
     vm.characters = accountResolver.data.characters;
     vm.characters.forEach(function(character) {
@@ -17,6 +19,7 @@
       character.characterBase.inventory = equipped.get(equipment, definitions);
     });
 
+    console.log(vm.account);
     console.log(vm.characters);
 
   }
